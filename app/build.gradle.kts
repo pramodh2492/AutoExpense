dfb1158ce40e6d71f8fccbcfe4095f3b4bf75081
Pramodh V <pramodhv@amazon.com>
2026-08-17 16:32:35 +0530

    Bump version to 1.1.6 (16)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.lazysloth.autoexpense"
         minSdk = 26
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.1.5"
+        versionCode = 16
+        versionName = "1.1.6"
     }
 
     buildTypes {
