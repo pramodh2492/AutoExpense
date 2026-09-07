2801605637d7f57f2978ea4a680d9b231ef8cd1b
Pramodh V <pramodhv@amazon.com>
2026-09-07 17:53:22 +0530

    Bump version to 1.2.4 (24)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.lazysloth.autoexpense"
         minSdk = 26
         targetSdk = 36
-        versionCode = 23
-        versionName = "1.2.3"
+        versionCode = 24
+        versionName = "1.2.4"
     }
 
     buildTypes {
